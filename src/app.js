ca54b10d8e48626cb852ca62b7cf0c9c606fcef3
Shaznay Sison <82836907+Shaznay-S@users.noreply.github.com>
2023-08-27 17:18:42 +0000

    finished and edited the project

--- src/app.js
+++ src/app.js
@@ -2,10 +2,56 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const who = [
+  "My pet rock",
+  "My clone",
+  "My imaginary friend",
+  "The president",
+  "Batman",
+  "The voices in my head",
+  "The CIA"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const what = [
+  "borrowed my time machine",
+  "challenged me to a duel",
+  "invited me to a dance battle",
+  "challenged me to a staring contest",
+  "held me hostage",
+  "ate my homework",
+  "invited me to explore",
+  "invited me to a tea party",
+  "proposed to me",
+  "invited me to a cult"
+];
+
+const where = [
+  "in a parallel universe",
+  "in an underground bunker",
+  "on the moon",
+  "yesterday",
+  "in Miami",
+  "this morning",
+  "in the Metaverse",
+  "in the Matrix",
+  "in a galaxy far, far away",
+  "in Gotham City"
+];
+
+const excuseSentence = document.getElementById("excuse");
+const generateButton = document.getElementById("generateButton");
+
+function generateExcuse() {
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhere = where[Math.floor(Math.random() * where.length)];
+
+  const newSentence = `${randomWho} ${randomWhat} ${randomWhere}`;
+  return newSentence;
+}
+
+generateButton.addEventListener("click", () => {
+  excuseSentence.textContent = generateExcuse();
+});
+
+excuseSentence.textContent = generateExcuse();
